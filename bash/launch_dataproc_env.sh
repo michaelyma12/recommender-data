4cef17791e3650de1995b587a88207478c6b11a2
Michael Ma <=>
2020-09-21 11:22:05 +0300

    Changed window spec

--- bash/launch_dataproc_env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-python generate_custom_image.py \
-    --image-name recommender-data-1 \
-    --dataproc-version "1.5.10-debian10" \
-    --customization-script startup_script/recommender_data_conda.sh \
-    --zone europe-west3-a \
-    --gcs-bucket gs://recommender-amazon-1
